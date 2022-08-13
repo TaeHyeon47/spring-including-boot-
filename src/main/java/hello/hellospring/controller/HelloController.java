29109c170253baaa53b42fe06562e25600015990
Tae Hyeon Jo <hillskuti@naver.com>
2022-08-13 18:21:31 +0900

    스프링 API 연습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +20,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // @ResponseBody는 HTML의 바디를 나타냄. 대부분 JSON으로 반환한다.
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(); // 객체가 오면 JSON으로 반환되는 것이 정책
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
     }
 }
