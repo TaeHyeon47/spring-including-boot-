0cd0417ecbce2fc2d883a919999ea9249cc67bbb
Tae Hyeon Jo <hillskuti@naver.com>
2022-08-13 17:05:19 +0900

    스프링 MVC 연습

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,13 +4,20 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+        model.addAttribute("data", "hello!!"); // 모델을 만듬.
+        return "hello"; // templates의 hello 파일을 찾아 모델을 전달하고 랜더링하라.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
